258d92eb45f30d21cd5a21e66b1a42fae1c46bdf
halenuryesilova <haleyesillova@gmail.com>
2025-04-18 10:06:09 +0300

    expo dependancies updated for simulation errors

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      'expo-router/babel', 
+      'react-native-reanimated/plugin',
+    ],
   };
+};
+
   
